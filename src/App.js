7d962a2f58d2e50873f27c02b156680f040af966
dafex301 <lfahrel@gmail.com>
2021-08-11 01:05:40 +0700

    Added blog details page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
 
     return (
         <div className="blog-list">
-            {blogs.map((blog) => (
-                <div className="blog-preview bg-white shadow-md m-7 p-5 rounded-lg border-t-4 hover:border-blue-400" key={blog.id}>
-                    <h2 className="text-blue-400 text-2xl font-semibold">{blog.title}</h2>
-                    <p className="text-gray-400">Written by {blog.author}</p>
-                </div>
+            {blogs.map((blog) => (             
+                    <Link to={`blogs/${blog.id}`}>
+                        <div className="blog-preview bg-white shadow-md m-7 p-5 rounded-lg border-t-4 hover:border-blue-400" key={blog.id}>
+                            <h2 className="text-blue-400 text-2xl font-semibold">{blog.title}</h2>
+                            <p className="text-gray-400">Written by {blog.author}</p>
+                        </div>
+                    </Link>
             ))} 
         </div>
     );
