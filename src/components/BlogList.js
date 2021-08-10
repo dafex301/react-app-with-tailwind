3938202622545d4d23ebbc5d725e40265acba81f
dafex301 <lfahrel@gmail.com>
2021-08-10 22:01:27 +0700

    Adding loading text if the fetched data not yet resolve.

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,7 +2,6 @@ const BlogList = ({ blogs, title }) => {
 
     return (
         <div className="blog-list">
-            <h2 className="font-sans m-7 text-2xl">{ title }</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview bg-white shadow-md m-7 p-5 rounded-lg border-t-4 hover:border-blue-400" key={blog.id}>
                     <h2 className="text-blue-400 text-2xl font-semibold">{blog.title}</h2>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,35 +3,26 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([]);
+    const [isLoading, setisLoading] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-            });
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            setisLoading(false);
+        });
     },[])
 
     return (
         <div className="container px-4 text-gray-600">
             <h1 className="text-center text-4xl m-5 font-bold border-b pb-3 border-gray-200">Homepage</h1>
-            <BlogList blogs={blogs} title="All Blogs!"/>
+            {isLoading && <p>Loading...</p>}
+            <BlogList blogs={blogs}/>
         </div>
     );
 }
 
 export default Home;
-
-// const [blogs, setBlogs] = useState([]);
-
-// useEffect(() => {
-//     fetch('http://localhost:8000/blogs')
-//         .then(res => {
-//             return res.json();
-//         })
-//         .then(data => {
-//             setBlogs(data);
-//         })
-// }, []);
