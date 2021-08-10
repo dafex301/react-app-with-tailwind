56065f47081f6438ed362f0f1ae993c42ffa29d0
dafex301 <lfahrel@gmail.com>
2021-08-10 20:06:03 +0700

    Experiment with useEffect function in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -9,15 +9,24 @@ const Home = () => {
         { title: 'Udud hese', body: 'lorem ipsum...', author: 'Mamat', id: 3 },
     ]);
 
+    const [name, setName] = useState(['Mario']);
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log('use effect fired!');
+        console.log({name});
+    }, [name]);
+
     return (
         <div className="container px-4 text-gray-600">
             <h1 className="text-center text-4xl m-5 font-bold border-b pb-3 border-gray-200">Homepage</h1>
             <BlogList blogs={blogs} title="All Blogs!" handleDelete = {handleDelete}/>
+            <h2 className="text-center">{ name }</h2>
+            <button className="bg-blue-400 p-2 text-white" onClick={() => setName('Mamat')}>Change name</button>
         </div>
     );
 }
