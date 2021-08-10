446870c89d305195ef36d5b510eacaa2502ee64a
dafex301 <lfahrel@gmail.com>
2021-08-11 00:01:13 +0700

    Making react router, learn about exact match router, and router links.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <div className="navbar flex items-center justify-between flex-wrap bg-blue-400 p-6">
-            <a className="font-semibold text-3xl tracking-tight text-white uppercase font-tourney" href="/">DaFeX</a>
+            <Link to="/" className="font-semibold text-3xl tracking-tight text-white uppercase font-tourney">DaFeX</Link>
             <div className="">
-                <a href="/" className="mr-8 text-white">Home</a>
-                <a href="/create" className="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 transition ease-out duration-300">New Blog</a>
+                <Link to="/" className="mr-8 text-white">Home</Link>
+                <Link to="/create" className="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 transition ease-out duration-300">New Blog</Link>
             </div>
         </div>
     );
