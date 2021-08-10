582c8a916a1e4ae6a9d717a31e72207859ab086a
dafex301 <lfahrel@gmail.com>
2021-08-10 22:46:37 +0700

    Revert "Handling fetch error"
    This reverts commit 3176fae78abac2afa58d51e3d008dd8053871a5f.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,33 +3,22 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(null);
-
+    const [isLoading, setisLoading] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs') // to run server 'npx json-server --watch data/db.json --port 8000'
+        fetch('http://localhost:8000/blogs')
         .then(res => {
-            if (!res.ok) {
-                throw Error('could not fetch from the resource.')
-            }
             return res.json();
         })
         .then(data => {
             setBlogs(data);
-            setIsLoading(false);
-            setIsError(false);
-        })
-        .catch(err => {
-            setIsError(err.message);
-            setIsLoading(false);
-        })
+            setisLoading(false);
+        });
     },[])
 
     return (
         <div className="container px-4 text-gray-600">
             <h1 className="text-center text-4xl m-5 font-bold border-b pb-3 border-gray-200">Homepage</h1>
-            {isError && <p>{isError}</p>}
             {isLoading && <p>Loading...</p>}
             <BlogList blogs={blogs}/>
         </div>
