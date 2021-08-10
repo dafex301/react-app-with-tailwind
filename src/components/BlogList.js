960d3ac72f3db67c15eb60f75adc8a7475d73c0d
dafex301 <lfahrel@gmail.com>
2021-08-10 19:33:51 +0700

    Reusing BookList component and using filter function.

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,11 +1,12 @@
-const BlogList = ({ blogs }) => {
+const BlogList = ({ blogs, title }) => {
 
     return (
         <div className="blog-list">
+            <h2 className="font-sans m-7 text-2xl">{ title }</h2>
             {blogs.map((blog) => (
-                <div className="blog-preview bg-white shadow-md m-7 p-5" key={blog.id}>
+                <div className="blog-preview bg-white shadow-md m-7 p-5 rounded-lg border-t-4 hover:border-blue-400" key={blog.id}>
                     <h2 className="text-blue-400 text-2xl font-semibold">{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
+                    <p className="text-gray-400">Written by {blog.author}</p>
                 </div>
             ))} 
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,14 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'Wuhan KNTL', body: 'lorem ipsum...', author: 'Mamat', id: 1 },
         { title: 'Madang hese', body: 'lorem ipsum...', author: 'Mario', id: 2 },
-        { title: 'Udud hese', body: 'lorem ipsum...', author: 'Lil Mamat', id: 3 },
+        { title: 'Udud hese', body: 'lorem ipsum...', author: 'Mamat', id: 3 },
     ]);
 
     return (
         <div className="container px-4 text-gray-600">
             <h1 className="text-center text-4xl m-5 font-bold border-b pb-3 border-gray-200">Homepage</h1>
-            <BlogList blogs={blogs} />
+            <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Mamat')} title="Mamat's Blog"/>
         </div>
     );
 }
