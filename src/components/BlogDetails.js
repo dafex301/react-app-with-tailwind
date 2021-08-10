e9dec0df56f97e79db5a9fa330a910eb1b9c5133
dafex301 <lfahrel@gmail.com>
2021-08-11 01:37:32 +0700

    Make a blog details page that contain the blog title, blog author, and blog description.

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const {data: blog, isLoading, isError} = useFetch('http://localhost:8000/blogs/' + id);
+
     return (  
-        <div className="bl">
-            <h2>Blog Details - {id}</h2>
+        <div className="blog-details mt-10 mx-24 border-2 p-6 rounded-xl drop-shadow-2xl">
+            {isError && <p>{isError}</p>}
+            {isLoading && <p>Loading...</p>}
+            {!isLoading && 
+            <article key={blog.id}>
+                <h2 className="text-blue-400 text-5xl font-semibold text-center">{blog.title}</h2>
+                <p className="text-gray-700 text-center text-xl mt-2 mb-4">Written by {blog.author}</p>
+                <p className="text-gray-700 text-justify">{blog.body}</p>
+            </article>
+            }
         </div>
     );
 }
- 
+
 export default BlogDetails;
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs }) => {
 
     return (
         <div className="blog-list">
             {blogs.map((blog) => (             
+                <div key={blog.id} className="blog-preview bg-white shadow-md m-7 rounded-lg border-t-4 hover:border-blue-400">
                     <Link to={`blogs/${blog.id}`}>
-                        <div className="blog-preview bg-white shadow-md m-7 p-5 rounded-lg border-t-4 hover:border-blue-400" key={blog.id}>
+                        <div className="w-full h-full p-5">
                             <h2 className="text-blue-400 text-2xl font-semibold">{blog.title}</h2>
                             <p className="text-gray-400">Written by {blog.author}</p>
                         </div>
                     </Link>
+                </div>
             ))} 
         </div>
     );
